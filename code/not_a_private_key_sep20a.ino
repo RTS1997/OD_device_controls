4467293c8ba3b8e5d2ba0bb692785283d19108cc
RTS1997 <reto.tschannen@unibas.ch>
2022-09-20 15:29:47 +0200

    feat: added OD reading script

---
+++ code/not_a_private_key_sep20a.ino
@@ -0,0 +1,22 @@
+const int readoutIN = A0;
+
+int sensorvalue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(readoutIN, INPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  sensorvalue = analogRead(readoutIN);
+  delay(5);
+
+  Serial.print(sensorvalue);
+  Serial.print("\n newline");
+}
